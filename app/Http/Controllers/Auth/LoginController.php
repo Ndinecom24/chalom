ffd998c5d6e545a94175639d131628b3b4982928
NShubart <nshubart@gmail.com>
2022-02-19 08:20:27 +0200

    home controller edit user login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Loans\LoanApplications;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,23 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        //check for uuid
+        if($request->uuid != 0){
+            // find the loan
+            $loan = LoanApplications::where('uuid', $request->uuid)->first();
+            $loan->customer_id = $user->id ;
+            $loan->statuses_id = config('constants.status.loan_request_login');
+            $loan->save();
+        }
     }
 }
