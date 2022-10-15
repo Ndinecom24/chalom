cbf5e0e712668dcbaa04c448e37ec18c5a34ade1
Peter Mudenda <pmudenda@zesco.co.zm>
2022-10-15 16:02:54 +0200

    updated datatable

--- resources/views/dashboard/loan/index.blade.php
+++ resources/views/dashboard/loan/index.blade.php
@@ -5,7 +5,6 @@
     <link rel="stylesheet"
           href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.css"/>
-    <link href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css">
     <div class="content-wrapper">
         <div class="row">
             <div class="container-fluid">
@@ -217,29 +216,13 @@
 
     <script src="//code.jquery.com/jquery-1.12.3.js"></script>
     <script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-
-    <script type="text/javascript"  src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript"  src=" https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script type="text/javascript"  src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-     <script type="text/javascript"  src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script type="text/javascript"  src=" https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
     <script
         src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.js"></script>
 
     <script>
-
-
         $(document).ready(function() {
-            $('#table').DataTable( {
-                dom: 'Bfrtip',
-                buttons: [
-                    'copyHtml5',
-                    'excelHtml5',
-                    'csvHtml5',
-                    'pdfHtml5'
-                ]
-            } );
+            $('#table').DataTable();
         } );
     </script>
 
