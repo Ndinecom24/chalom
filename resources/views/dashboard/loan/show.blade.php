f01526ba1969c0db2b19413638e9e4dec025e472
Nimbus <NShubart@zesco.co.zm>
2023-04-15 14:02:19 +0200

    t

--- resources/views/dashboard/loan/show.blade.php
+++ resources/views/dashboard/loan/show.blade.php
@@ -463,7 +463,7 @@
                                                                 </label>
                                                         </div>
                                                     </div>
-
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
